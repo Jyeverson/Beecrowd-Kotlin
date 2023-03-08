498e5650d09752a3689db9f1048531671e791d4e
jyeverson <jyeversonsantos@gmail.com>
2023-03-08 14:05:39 -0300

    novo exercicio resolvido

--- src/_1011.kt
+++ src/_1011.kt
@@ -2,7 +2,7 @@ import java.util.Scanner;
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
     val PI = 3.14159
-    val R: Double = sc.nextInt()
+    val R = sc.nextInt()
     val volume = 4 / 3.0 * PI * (R * R * R)
     System.out.printf(String.format("VOLUME = %.3f", volume))
     println()
